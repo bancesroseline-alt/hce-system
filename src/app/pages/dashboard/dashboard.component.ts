9d96b891472422ab10b1ff06ff4e10bdd2ef191f
bancesroseline-alt <bancesroseline@gmail.com>
2026-06-09 17:25:06 -0600

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,3 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
 export class DashboardComponent implements OnInit {
 
   totalPacientes = 0;
