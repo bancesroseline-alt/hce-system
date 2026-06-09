3932dac1d757714b895e52a05d512de823b34759
bancesroseline-alt <bancesroseline@gmail.com>
2026-06-09 16:01:52 -0600

    Update dashboard.service.ts

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -6,19 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class DashboardService {
 
-  private api = 'https://hce-backend.onrender.com/api';
+  private api = 'https://hce-backend.onrender.com/api/dashboard';
 
   constructor(private http: HttpClient) {}
 
-  getTotalPacientes() {
-    return this.http.get<number>(`${this.api}/pacientes/total`);
+  obtenerDashboardMedico(medicoId: number) {
+    return this.http.get<any>(`${this.api}/medico/${medicoId}`);
   }
-
-  getTotalCitas() {
-    return this.http.get<number>(`${this.api}/citas/total`);
-  }
-
-  getCitasHoy() {
-    return this.http.get<number>(`${this.api}/citas/hoy`);
-  }
+}
 }
