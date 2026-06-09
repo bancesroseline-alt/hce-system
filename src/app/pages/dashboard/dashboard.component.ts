f3ca0af88df7de8eb64f4b2db143d2d3de10c022
bancesroseline-alt <bancesroseline@gmail.com>
2026-06-09 16:02:18 -0600

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,32 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
   totalPacientes = 0;
-  totalCitas = 0;
-  citasHoy = 0;
+  totalCitasHoy = 0;
+  porcentajeInasistencia = 0;
+
+  citasDelDia: any[] = [];
+  actividadesRecientes: any[] = [];
+
+  usuario: any;
 
   constructor(private dashboardService: DashboardService) {}
 
   ngOnInit(): void {
-    this.cargarDatos();
+    const usuarioStorage = localStorage.getItem('usuario');
+
+    if (usuarioStorage) {
+      this.usuario = JSON.parse(usuarioStorage);
+      this.cargarDashboard();
+    }
   }
 
-  cargarDatos() {
-    this.dashboardService.getTotalPacientes()
-      .subscribe(data => this.totalPacientes = data);
-
-    this.dashboardService.getTotalCitas()
-      .subscribe(data => this.totalCitas = data);
-
-    this.dashboardService.getCitasHoy()
-      .subscribe(data => this.citasHoy = data);
+  cargarDashboard(): void {
+    this.dashboardService.obtenerDashboardMedico(this.usuario.id).subscribe({
+      next: (data) => {
+        this.totalPacientes = data.totalPacientes;
+        this.totalCitasHoy = data.totalCitasHoy;
+        this.porcentajeInasistencia = data.porcentajeInasistencia;
+        this.citasDelDia = data.citasDelDia;
+        this.actividadesRecientes = data.actividadesRecientes;
+      },
+      error: (err) => {
+        console.error('Error al cargar dashboard', err);
+      }
+    });
   }
 }
