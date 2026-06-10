c591283cff60c9853f566d47f666fbcc0875956b
bancesroseline-alt <bancesroseline@gmail.com>
2026-06-09 20:51:57 -0500

    Update citas.component.ts

--- src/app/pages/citas/citas.component.ts
+++ src/app/pages/citas/citas.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { CitaService } from '../../services/cita.service';
+import { Cita } from '../../models/cita.model';
 
 @Component({
   selector: 'app-citas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './citas.component.html',
-  styleUrl: './citas.component.css'
+  styleUrls: ['./citas.component.css']
 })
 export class CitasComponent {
 
+  citas: Cita[] = [];
+  idPaciente: number | null = null;
+
+  constructor(
+    private citaService: CitaService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.idPaciente = Number(id);
+
+      this.citaService.porPaciente(this.idPaciente)
+        .subscribe(data => {
+          this.citas = data ?? [];
+        });
+
+    } else {
+      this.citaService.listar()
+        .subscribe(data => {
+          this.citas = data ?? [];
+        });
+    }
+  }
 }
