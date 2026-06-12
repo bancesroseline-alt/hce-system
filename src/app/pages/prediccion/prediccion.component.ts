a9d13a867709f47be1daec37c7c4887f5f90bb56
bancesroseline-alt <bancesroseline@gmail.com>
2026-06-12 18:11:53 -0500

    Update prediccion.component.ts

--- src/app/pages/prediccion/prediccion.component.ts
+++ src/app/pages/prediccion/prediccion.component.ts
@@ -1,43 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PrediccionService } from '../../services/prediccion.service';
+import { PrediccionService } from 'src/app/services/prediccion.service';
 
 @Component({
   selector: 'app-prediccion',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './prediccion.component.html',
   styleUrls: ['./prediccion.component.css']
 })
 export class PrediccionComponent implements OnInit {
 
   alertas: any[] = [];
+
   totalAlertas = 0;
+  alto = 0;
+  bajo = 0;
 
-  alertasAltas: any[] = [];
-  alertasMedias: any[] = [];
-  alertasBajas: any[] = [];
-
-  constructor(private prediccionService: PrediccionService) {}
+  constructor(private service: PrediccionService) {}
 
   ngOnInit(): void {
     this.cargarAlertas();
   }
 
   cargarAlertas(): void {
-    this.prediccionService.obtenerAlertas().subscribe({
-      next: (data) => {
+    this.service.obtenerAlertas().subscribe(data => {
+      this.alertas = data || [];
 
-        this.alertas = data || [];
-        this.totalAlertas = this.alertas.length;
+      this.totalAlertas = this.alertas.length;
 
-        this.alertasAltas = this.alertas.filter(a => a.nivelRiesgo === 'ALTO');
-        this.alertasMedias = this.alertas.filter(a => a.nivelRiesgo === 'MEDIO');
-        this.alertasBajas = this.alertas.filter(a => a.nivelRiesgo === 'BAJO');
-      },
-      error: (err) => {
-        console.error(err);
-      }
+      this.alto = this.alertas.filter(a => a.nivelRiesgo === 'ALTO').length;
+      this.bajo = this.alertas.filter(a => a.nivelRiesgo === 'BAJO').length;
     });
   }
 }
